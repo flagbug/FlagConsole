a7602dda55fd82cd321d866a3cea1061cdb19e30
Dennis Daume <daume.dennis@gmail.com>
2011-09-03 15:00:43 +0200

    Created basic structure of the graphic buffer

--- FlagConsole/FlagConsole/Drawing/GraphicBuffer.cs
+++ FlagConsole/FlagConsole/Drawing/GraphicBuffer.cs
@@ -1,11 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace FlagConsole.Drawing
+﻿namespace FlagConsole.Drawing
 {
-    class GraphicBuffer
+    public class GraphicBuffer
     {
+        public char[,] buffer;
+
+        /// <summary>
+        /// Gets the width of this graphic buffer.
+        /// </summary>
+        public int Width { get; private set; }
+
+        /// <summary>
+        /// Gets the height of this graphic buffer.
+        /// </summary>
+        public int Height { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GraphicBuffer"/> class.
+        /// </summary>
+        /// <param name="width">The width of the buffer.</param>
+        /// <param name="height">The height of the buffer.</param>
+        public GraphicBuffer(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.buffer = new char[width, height];
+        }
     }
 }
