bfd0dc74198704140de4e7fa07b5a69ca931055c
Dennis Daume <daume.dennis@gmail.com>
2011-09-03 19:09:06 +0200

    Added the "Pixel" class, which encapsulates a character, a foregorund color and a background color
    Added color support for the "DrawToScreen" method

---
+++ FlagConsole/FlagConsole/Drawing/Pixel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FlagConsole.Drawing
+{
+    internal class Pixel
+    {
+        /// <summary>
+        /// Gets the token.
+        /// </summary>
+        public char Token { get; private set; }
+
+        /// <summary>
+        /// Gets the foreground color.
+        /// </summary>
+        /// <value>
+        /// The foreground color.
+        /// </value>
+        public ConsoleColor ForegroundColor { get; private set; }
+
+        /// <summary>
+        /// Gets the background color.
+        /// </summary>
+        /// <value>
+        /// The background color.
+        /// </value>
+        public ConsoleColor BackgroundColor { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Pixel"/> class.
+        /// </summary>
+        /// <param name="token">The token.</param>
+        /// <param name="foregroundColor">The foreground color.</param>
+        /// <param name="backgroundColor">The background color.</param>
+        public Pixel(char token, ConsoleColor foregroundColor, ConsoleColor backgroundColor)
+        {
+            this.Token = token;
+            this.ForegroundColor = foregroundColor;
+            this.BackgroundColor = backgroundColor;
+        }
+    }
+}
