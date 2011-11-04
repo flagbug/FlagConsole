2b3a171b729ee89c8952945968da123af67b1ba4
Dennis Daume <daume.dennis@gmail.com>
2011-11-04 20:55:24 +0100

    Updated the demo

--- FlagConsole/FlagConsole.Demo/RectangleDemoPanel.cs
+++ FlagConsole/FlagConsole.Demo/RectangleDemoPanel.cs
@@ -10,12 +10,12 @@ namespace FlagConsole.Demo
 
         public RectangleDemoPanel()
         {
-            this.filledRectangleLabel = new Label { Text = "This is a filled rectangle" };
+            this.filledRectangleLabel = new Label { Text = "This is a filled rectangle." };
             this.filledRectangleLabel.Size = new Size(this.filledRectangleLabel.Text.Length, 1);
             this.filledRectangleLabel.RelativeLocation = new Coordinate(17, 0);
             this.Controls.Add(this.filledRectangleLabel);
 
-            this.emptyRectangleLabel = new Label { Text = "This is an empty rectangle" };
+            this.emptyRectangleLabel = new Label { Text = "This is an empty rectangle." };
             this.emptyRectangleLabel.Size = new Size(this.emptyRectangleLabel.Text.Length, 1);
             this.emptyRectangleLabel.RelativeLocation = new Coordinate(17, 11);
             this.Controls.Add(this.emptyRectangleLabel);
