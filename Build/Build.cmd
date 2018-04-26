79793e0fe923ac78dada49c3f3b4b34ef0760df4
kyates <kyates@blizzard.com>
2018-04-26 16:12:10 -0700

    - removing x86 platform (it's unneeded) - cleaning up the projects so they output to a common location (for easier cleanup) - updating Build/Build.cmd to use the new output location

--- Build/Build.cmd
+++ Build/Build.cmd
@@ -21,13 +21,13 @@ msbuild.exe /ToolsVersion:12.0 "..\FlagConsole\FlagConsole.sln" /p:configuration
 mkdir ..\Release\Library
 mkdir ..\Release\Demo
 
-copy ..\FlagConsole\FlagConsole\bin\Release\FlagConsole.dll ..\Release\Library\FlagConsole.dll
-copy ..\FlagConsole\FlagConsole\bin\Release\FlagConsole.xml ..\Release\Library\FlagConsole.xml
+copy ..\FlagConsole\Build\FlagConsole\AnyCPU\Release\bin\FlagConsole.dll ..\Release\Library\FlagConsole.dll
+copy ..\FlagConsole\Build\FlagConsole\AnyCPU\Release\bin\FlagConsole.xml ..\Release\Library\FlagConsole.xml
 copy ..\Changelog.txt ..\Release\Library\Changelog.txt
 
-copy ..\FlagConsole\FlagConsole\bin\Release\FlagConsole.dll ..\Release\Demo\FlagConsole.dll
-copy ..\FlagConsole\FlagConsole\bin\Release\FlagConsole.xml ..\Release\Demo\FlagConsole.xml
-copy ..\FlagConsole\FlagConsole.Demo\bin\Release\FlagConsole.Demo.exe ..\Release\Demo\FlagConsole.Demo.exe
+copy ..\FlagConsole\Build\FlagConsole\AnyCPU\Release\bin\FlagConsole.dll ..\Release\Demo\FlagConsole.dll
+copy ..\FlagConsole\Build\FlagConsole\AnyCPU\Release\bin\FlagConsole.xml ..\Release\Demo\FlagConsole.xml
+copy ..\FlagConsole\Build\FlagConsole.Demo\AnyCPU\Release\bin\FlagConsole.Demo.exe ..\Release\Demo\FlagConsole.Demo.exe
 copy ..\Changelog.txt ..\Release\Demo\Changelog.txt
 
 pause
