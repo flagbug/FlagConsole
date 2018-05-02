b2e93d38ea83c529c53bb132cae89b350e559f11
kyates <kyates@blizzard.com>
2018-05-02 12:48:43 -0700

    adding SubScreen

---
+++ FlagConsole/FlagConsole/Controls/SubScreen.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+
+namespace FlagConsole.Controls
+{
+    internal class SubScreen : Screen
+    {
+        #region Fields
+
+        private readonly Screen parent;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        public SubScreen(Screen parent)
+        {
+            this.parent = parent;
+        }
+
+        #endregion
+
+        public override void Hide()
+        {
+            base.Hide();
+            this.parent.Show();
+        }
+
+        public override void Show()
+        {
+            this.parent.Hide();
+            base.Show();
+        }
+    }
+}
